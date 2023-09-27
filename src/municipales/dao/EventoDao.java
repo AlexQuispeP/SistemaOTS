a3064a9c1f031134695108719175649763c6790f
AlexQuispeP <136035598+AlexQuispeP@users.noreply.github.com>
2023-09-27 11:35:14 -0300

    feat(Eventos)

---
+++ src/municipales/dao/EventoDao.java
@@ -0,0 +1,133 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import modelo.Evento;
+
+
+public class EventoDAO extends Conexion{
+	
+	public boolean agregar(Evento e) {
+		int filasAfectadas = 0;
+		Connection c = null;
+		try {
+			c = conectar();
+			String Sql = "INSERT INTO `mydb`.`evento`(`titulo_Evento`,`fecha_Evento`,`hora_inicio`,`hora_Fin`,`motivo_Evento`)VALUES(?, ?, ?, ?, ?)";
+			PreparedStatement pst = c.prepareStatement(Sql);
+
+			pst.setString(1, e.getNombreEvento());
+			pst.setDate(2, new java.sql.Date(e.getFechaEvento().getTime()));
+			pst.setInt(3, e.getHoraInicio());
+			pst.setInt(4, e.getHoraFin());
+			pst.setString(5, e.getMotivoEvento());
+			pst.executeUpdate();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return filasAfectadas != 0;
+	}
+	
+	public boolean Eliminar(String eventoName) {
+		int filasAfectadas = 0;
+		Connection c = null;
+		try {
+			c = conectar();
+			String sql = "DELETE FROM `mydb`.`evento` WHERE titulo_Evento = ?";
+			PreparedStatement pst = c.prepareStatement(sql);
+			pst.setString(1, eventoName);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return filasAfectadas != 0;
+	}
+	public boolean Modificar(String titulo, Evento e) {
+		int filasAfectadas = 0;
+		Connection c = null;
+		try {
+			c = conectar();
+			String Sql = "UPDATE `mydb`.`evento` SET`titulo_Evento` = ?,`fecha_Evento` = ? ,`hora_inicio` = ?,`hora_Fin` = ?,`motivo_Evento` = ? WHERE `titulo_Evento` = ? ";
+			PreparedStatement pst = c.prepareStatement(Sql);
+			pst.setString(1, e.getNombreEvento());
+			pst.setDate(2, new java.sql.Date(e.getFechaEvento().getTime()));
+			pst.setInt(3, e.getHoraInicio());
+			pst.setInt(4, e.getHoraFin());
+			pst.setString(5, e.getMotivoEvento());
+			pst.setString(6, titulo);
+			
+			filasAfectadas = pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return filasAfectadas != 0;
+	}
+	public ArrayList<Evento> traerTodas() {
+		// consultaSinParametros = SELECT *;o
+		Connection c = null;
+		ArrayList<Evento> eventos = new ArrayList<Evento>();
+		try {
+			c = conectar();
+			String sql = "SELECT `titulo_Evento`,`fecha_Evento`,`hora_inicio`,`hora_Fin`,`motivo_Evento` FROM `mydb`.evento ;";
+			Statement st = c.createStatement();
+
+			ResultSet rs = st.executeQuery(sql);
+			categoriaDAO ca = new categoriaDAO();
+			while (rs.next()) {
+
+				String titulo = rs.getString("titulo_Evento");
+				java.sql.Date fecha = rs.getDate("fecha_Evento");;
+				int horaIn = rs.getInt("hora_inicio");
+				int horaFin = rs.getInt("hora_Fin");
+				String motivo_Evento = rs.getString("motivo_Evento");
+
+				Evento e = new Evento(titulo,fecha,horaIn,horaFin,motivo_Evento);
+				eventos.add(e);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return eventos;
+	}
+
+}
